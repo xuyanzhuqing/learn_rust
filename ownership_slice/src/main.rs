fd48225a05749475016896ac9b7e0bfbef15b0dd
michael <962275976@qq.com>
2025-08-15 16:06:54 +0800

    slice

--- ownership_slice/src/main.rs
+++ ownership_slice/src/main.rs
@@ -5,12 +5,15 @@ fn main() {
     util::tester(
         vec![
             Box::new(|| {
-                println!("执行动作 1");
-                println!("执行动作 2");
+                let tip = String::from("hello world");
+                let len = tip.len();
+                println!("{}, {}, {}", &tip[..], &tip[0..], &tip[..len]);
+
+                println!("{}", &tip[2..10]);
             }),
             Box::new(|| {
-                println!("执行动作 3");
-                println!("执行动作 4");
+                let arr = [1, 2, 3, 4, 5];
+                println!("{:#?}", &arr[2..4]);
             }),
         ],
         None,
