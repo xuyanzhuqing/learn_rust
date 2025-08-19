f007c4b725b10d2d6e2e6d103735d4fdc5870526
michael <962275976@qq.com>
2025-08-19 13:30:56 +0800

    rust to wasm for javascript import

---
+++ util_wasm/src/lib.rs
@@ -0,0 +1,18 @@
+use wasm_bindgen::prelude::*;
+
+// 标记导出 javascript 可调用
+#[wasm_bindgen]
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
